cbb4179d569d3764f5b78fd3aa3d5a94f927d58c
chenmh <chenmh@sucsoft.com>
2022-04-28 15:44:35 +0800

    chore: init structure

--- index.ts
+++ index.ts
@@ -1,2 +1,35 @@
-// deps
-// const
+// region types
+import type { IOType } from "./types/io";
+import type { Express } from "express";
+// endregion
+
+// region deps
+// import * as express from "express";
+const express = require("express");
+import { Server } from "http";
+import { Server as IOServer } from "socket.io";
+import { setupIO } from "./src/io";
+import { setupExpress } from "./src/express";
+// endregion
+
+// config
+const HOST = '127.0.0.1'
+const PORT = 8899
+// endregion
+
+// middleware
+const app: Express = express()
+const http = new Server(app)
+const io: IOType = new IOServer(http)
+// endregion
+
+// region bind event
+setupIO(io)
+setupExpress(app)
+// endregion
+
+// region start
+http.listen(PORT, HOST, () => {
+    console.log(`server run at ${ HOST }:${ PORT }`)
+})
+// endregion
