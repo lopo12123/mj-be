7dd02918ce46d8e496a2c354519de67774a79e4b
lopo <lopo@zju.edu.cn>
2022-04-29 16:26:42 +0800

    refactor: ping-pong 测试

--- src/express/index.ts
+++ src/express/index.ts
@@ -1,11 +1,15 @@
 import { Express } from "express";
 import * as path from "path";
 import apiRouter from "./api";
+import { v4 } from "uuid";
+
+const session_timeout = 30 * 60 * 1000  // 60min
 
 const setupExpress = (ex: Express) => {
     // region 设置请求头 允许跨域
     ex.all('*', (req, res, next) => {
         res.header('Access-Control-Allow-Origin', req.hostname)
+        res.cookie('SESSIONID', (req.cookies.SESSIONID ?? v4()), { maxAge: session_timeout })
         next()
     })
     // endregion
@@ -17,7 +21,7 @@ const setupExpress = (ex: Express) => {
     // endregion
 
     // region 测试 ping-pong
-    apiRouter.get('/ping', (req, res) => {
+    ex.get('/ping', (req, res) => {
         res.json({ msg: 'pong' })
     })
     // endregion
