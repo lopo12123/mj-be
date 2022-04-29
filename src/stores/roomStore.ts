f1d7633ae470dace9c17b5fcb328bf7fa88b4a48
lopo <lopo@zju.edu.cn>
2022-04-29 18:03:14 +0800

    feat: 存储房间和clients

--- src/stores/roomStore.ts
+++ src/stores/roomStore.ts
@@ -1,71 +1,83 @@
 import { v4 as UUID } from "uuid";
 
 class SingleRoom {
-    roomId: string
+    private readonly roomId: string
+    private clients: string[] = []
 
-    info: [string, string, string, string]
-
-    constructor() {
-        this.roomId = UUID()
-        this.info = ['', '', '', '']
+    get id() {
+        return this.roomId
     }
 
+    constructor(clientId: string) {
+        this.roomId = UUID()
+        this.clients.push(clientId)
+    }
+
+    /**
+     * @description 返回当前的client数量
+     */
     count() {
-        let count = 0
-        for(let i = 0; i < 4; i ++) {
-            if(this.info[i] !== '') count += 1
-        }
-        return count
+        return this.clients.length
     }
 
     /**
      * @description 添加一个连接, 并返回加入的位置
      */
-    add(sessionId: string) {
-        for(let i = 0; i < 4; i ++) {
-            if(this.info[i] === '') {
-                this.info[i] = sessionId
-                return [true, i]
-            }
-        }
-        return [false, -1]
+    add(clientId: string) {
+        this.clients.push(clientId)
     }
 
     /**
-     * @description 指定设置某个位置的连接
+     * @description 将目标client移除
+     * @param clientId
      */
-    set(index: number, sessionId: string) {
-        this.info[index] = sessionId
+    remove(clientId: string) {
+        const index = this.clients.findIndex((client) => client === clientId)
+        if(index !== -1) this.clients.splice(index, 1)
     }
 }
 
 class RoomStore {
     private roomList: Map<string, SingleRoom> = new Map()
 
+    /**
+     * @description 自动清理没有client的room
+     */
     private auto_clean() {
-        for(let [k, v] of this.roomList) {
+        for (let [ k, v ] of this.roomList) {
             if(v.count() === 0) this.roomList.delete(k)
         }
     }
 
-    newRoom(sessionId: string) {
+    /**
+     * @description 新建room
+     */
+    newRoom(clientId: string) {
         this.auto_clean()
-        const newRoom = new SingleRoom()
-        newRoom.add(sessionId)
-        this.roomList.set(newRoom.roomId, newRoom)
-        return newRoom.roomId
+        const newRoom = new SingleRoom(clientId)
+        this.roomList.set(newRoom.id, newRoom)
+        return newRoom.id
     }
 
-    has(roomId: string) {
+    /**
+     * @description 判断目标room是否存在
+     */
+    checkRoom(roomId: string) {
         this.auto_clean()
         return this.roomList.has(roomId)
     }
 
-    get(roomId: string) {
+    /**
+     * @description 获取目标room, 不存在则返回null
+     */
+    getRoom(roomId: string) {
         this.auto_clean()
         return this.roomList.get(roomId) ?? null
     }
 
+    /**
+     * @description 返回room数量
+     */
     count() {
         this.auto_clean()
         return this.roomList.size
