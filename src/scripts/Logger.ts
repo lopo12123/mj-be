a2f65a0d1a203a9bd20f30c022d2fe5a458e7e21
lopo <lopo@zju.edu.cn>
2022-04-29 15:42:02 +0800

    style: log样式

--- src/scripts/Logger.ts
+++ src/scripts/Logger.ts
@@ -1,4 +1,4 @@
 export const Logger = (msg: string, title: string = '') => {
     const timestamp = new Date().toLocaleString('zh-cn', { hour12: false })
-    console.log(`[${ timestamp }]: ${ title ? '<' + title + '>' : '' }${ msg }`)
+    console.log(`[${ timestamp }]: ${ title ? '<' + title + '> ' : '' }${ msg }`)
 }
