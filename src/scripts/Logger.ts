ad82b712646a3850823e93202871df6c5e2d75e7
lopo <lopo@zju.edu.cn>
2022-04-29 11:34:09 +0800

    feat: 24小时制显示时间戳

--- src/scripts/Logger.ts
+++ src/scripts/Logger.ts
@@ -1,4 +1,4 @@
 export const Logger = (msg: string, title: string = '') => {
-    const timestamp = new Date().toLocaleString()
+    const timestamp = new Date().toLocaleString('zh-cn', {hour12: false})
     console.log(`[${timestamp}]: ${title} ${msg}`)
 }
